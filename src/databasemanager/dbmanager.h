24a25d1a5bb6b582c0b9dccccfeb7e76cdd11841
lixh <179685239@qq.com>
2020-09-14 23:25:23 +0800

    create database manager

---
+++ src/databasemanager/dbmanager.h
@@ -0,0 +1,15 @@
+#ifndef DBMANAGER_H
+#define DBMANAGER_H
+#include <QObject>
+
+/**
+ * @brief The DbManager class database management
+ */
+class DbManager : public QObject
+{
+    Q_OBJECT
+public:
+    DbManager();
+};
+
+#endif // DBMANAGER_H
